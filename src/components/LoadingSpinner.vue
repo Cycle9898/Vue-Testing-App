aeadfe19a8507345df04704590ab28981d88f9a7
Cycle9898 <cycle9898@gmail.com>
2025-04-04 19:01:29 +0200

    experiment Vue async components

---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,41 @@
+<template>
+	<div class="container">
+		<div class="spinner">
+			<div class="spinner-center"></div>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+.container {
+	--size: 30px;
+	--diff: 10px;
+	position: relative;
+}
+
+.spinner {
+	width: var(--size);
+	height: var(--size);
+	border-radius: 50%;
+	background: conic-gradient(var(--vue-green), var(--vt-c-text-dark-2));
+	border: 1px solid transparent;
+	animation: spinner-kf linear 1s infinite both;
+}
+
+.spinner-center {
+	background-color: var(--color-background);
+	border-radius: 50%;
+	width: calc(var(--size) - var(--diff));
+	height: calc(var(--size) - var(--diff));
+	position: absolute;
+	top: calc(50% - (var(--size) / 2 - (var(--diff) / 2)));
+	left: calc(50% - (var(--size) / 2 - (var(--diff) / 2)));
+	z-index: 1;
+}
+
+@keyframes spinner-kf {
+	100% {
+		transform: rotate(360deg);
+	}
+}
+</style>
--- src/components/VueIconSvgHelper.vue
+++ src/components/VueIconSvgHelper.vue
@@ -1,17 +1,23 @@
 <script setup lang="ts">
 import { defineAsyncComponent } from "vue";
+import IconSvgError from "./icons/IconSvgError.vue";
+import LoadingSpinner from "./LoadingSpinner.vue";
 
 const { name } = defineProps<{
 	name: string;
 }>();
 
-const svgComponent = defineAsyncComponent(async () => {
-	const iconName = name[0].toUpperCase() + name.slice(1);
+const SvgIcon = defineAsyncComponent({
+	loader: () => {
+		const iconName = name[0].toUpperCase() + name.slice(1);
 
-	return await import(`./icons/${iconName}.vue`);
+		return import(`./icons/${iconName}.vue`);
+	},
+	errorComponent: IconSvgError,
+	loadingComponent: LoadingSpinner
 });
 </script>
 
 <template>
-	<component :is="svgComponent" />
+	<component :is="SvgIcon" />
 </template>
